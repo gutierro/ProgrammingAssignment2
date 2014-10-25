f37c54bf396bc5e1eed0ce2d1846b419811700c7
gutierro <roberto_gutierrez_fernandez@hotmail.com>
2014-10-25 23:46:57 +0200

    version 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,11 @@
 
-## Funtion to create the matrix object.
-## This i
+## It creates an object with 4 functions and 2 variables (matrix and the inverse).
 makeCacheMatrix <- function(x = matrix()) {
         
-        ## Initialize the inverse in the current environment
+        ## initializating the inverse.
         m <- NULL
         
-        ## it sets the matrix object 'x' and initializes the inverse matrix 'm'.
+        ## it sets the matrix object 'x' and initializes the inverse matrix 'm' (in the parent environment).
         set <- function(y) {
                 x <<- y
                 m <<- NULL
@@ -15,7 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
         ## it gets the matrix object 'x'
         get <- function() x
         
-        ## it sets the inverse matrix 'm'.
+        ## it sets the inverse matrix 'm' (in the parent environment).
         setInverse <- function(inverse) m <<- inverse
         
         ## it gets the inverse matrix 'm'.
@@ -27,7 +26,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## It returns the inverse of the special matrix created with the function object makeCacheMatrix
+## It returns the inverse of the special matrix object created with the function makeCacheMatrix
 cacheSolve <- function(x, ...) {
         
         ## it gets the inverse from the cached object 'x' (special matrix) and checks if the inverse object 'x' is empty..
@@ -37,10 +36,10 @@ cacheSolve <- function(x, ...) {
                 return(m)
         }
         ## If the inverse variable in the cached object 'x' is empty, 
-        ## it gets matrix value from object 'x' 
+        ## it gets the matrix value from object 'x' 
         data <- x$get()
         
-        ## It calculates the inverse and save it in the object 'x' for next time .
+        ## it calculates the inverse and save it in the object 'x' for next time .
         m <- solve(data)
         x$setInverse(m)
         m
