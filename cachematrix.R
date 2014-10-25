eb1595c2b6b8a9d3862d97a88819076d382ab73a
gutierro <roberto_gutierrez_fernandez@hotmail.com>
2014-10-25 23:25:44 +0200

    version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## Funtion to create the matrix object.
+## This i
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## Initialize the inverse in the current environment
+        m <- NULL
+        
+        ## Funtion to set the Matrix and to initialize the inverse matrix 'm'.
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## Funtion to get the Matrix
+        get <- function() x
+        
+        ## Funtion to set the inverse matrix 'm'.
+        setInverse <- function(inverse) m <<- inverse
+        
+        ## Get the inverse matrix 'm'.
+        getInverse <- function() m
+        
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)       
 }
 
 
-## Write a short comment describing this function
-
+## It returns the inverse of the special matrix created with the function object makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Gets the inverse from the cached object 'x' and checks if not empty inverser.
+        m <- x$getInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        ## If the cached object 'x' doesn't have and inverse then calculates the inverse and save it in the object 'x'.
+        data <- x$get()
+        m <- solve(data)
+        x$setInverse(m)
+        m
 }
+
+## Example run:
+## > mdat<-matrix(c(1 ,1, 1, 1, 1, 2, 1, 2 ,1, 1, 1, 0 ,1, 4, 2, 3),nrow=4,ncol=4)
+## > m = makeCacheMatrix(mdat)
+# > m$get()
+# [,1] [,2] [,3] [,4]
+# [1,]    1    1    1    1
+# [2,]    1    2    1    4
+# [3,]    1    1    1    2
+# [4,]    1    2    0    3
+# > cacheSolve(m)
+# [,1] [,2] [,3] [,4]
+# [1,]   -1   -2    3    1
+# [2,]    2    1   -3    0
+# [3,]    1    1   -1   -1
+# [4,]   -1    0    1    0
